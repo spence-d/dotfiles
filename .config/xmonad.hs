5234589ad81fecbbc0959ca775f271e10738c24f
Spencer Dailey <j.spencer.dailey@gmail.com>
2021-06-16 01:57:52 -0500

    xmonad tweaks

--- .config/xmonad.hs
+++ .config/xmonad.hs
@@ -1,17 +1,21 @@
 import XMonad
+import qualified XMonad.StackSet as W
 import XMonad.Util.SpawnOnce
 import XMonad.Util.EZConfig
 import XMonad.Layout.Gaps
 import XMonad.Layout.Spacing
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.Grid
+import XMonad.Layout.Spiral
+import XMonad.Layout.MultiColumns
 import XMonad.Actions.GroupNavigation
 import XMonad.Actions.Navigation2D
 import XMonad.Actions.WindowGo
 
-myLayoutHook = gapped ||| Mirror gapped ||| Grid ||| Full
+myLayoutHook = gapped tiled ||| gapped (multiCol [1] 1 0.01 (-0.5)) |||
+               gapped Grid ||| gapped (spiral (6/7)) ||| fullscreenFull Full
   where tiled = Tall 1 (3/100) (1/2)
-        gapped = spacing 5 $ gaps [(U,8), (R,8), (L,8), (D,8)] tiled
+        gapped = \x -> spacing 5 $ gaps [(U,8), (R,8), (L,8), (D,8)] x
 
 modm = mod4Mask
 
@@ -34,7 +38,7 @@ main = xmonad $ fullscreenSupport $ def
   `additionalKeys`
     [
       --Spawn keys
-      ((modm, xK_p), spawn "rofi -show combi"),
+      ((modm, xK_space), spawn "rofi -show combi"),
       ((modm, xK_b), spawn "vivaldi-stable"),
       ((modm .|. shiftMask, xK_b), runOrRaiseNext "vivaldi-stable" (resource =? "vivaldi-stable")),
       ((modm, xK_Return), spawn "kitty"),
@@ -51,5 +55,21 @@ main = xmonad $ fullscreenSupport $ def
       ((modm .|. shiftMask, xK_l), windowSwap R False),
 
       --Toggle back and forth between two recent windows
-      ((modm, xK_BackSpace), nextMatch History (return True))
+      ((modm, xK_BackSpace), nextMatch History (return True)),
+
+      --Resize master
+      ((modm .|. shiftMask, xK_equal), sendMessage Expand),
+      ((modm, xK_hyphen), sendMessage Shrink),
+
+      --Cycle layouts
+      ((modm .|. shiftMask, xK_space), sendMessage NextLayout)
+    ]
+  `additionalMouseBindings`
+    [
+      --Cycle through windows with scroll wheel
+      ((modm, button4), \w -> focus w >> windows W.focusUp),
+      ((modm, button5), \w -> focus w >> windows W.focusDown),
+      --Resize master area with scroll wheel
+      ((modm .|. shiftMask, button4), \w -> focus w >> sendMessage Expand),
+      ((modm .|. shiftMask, button5), \w -> focus w >> sendMessage Shrink)
     ]
